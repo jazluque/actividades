8c524b145b149c910b49e1a6e9273f924c29f31e
jazluque <81364412+jazluque@users.noreply.github.com>
2021-03-25 09:45:13 -0300

    Add files via upload

---
+++ ejer2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jazluque2021;
+
+/**
+ *
+ * @author pc10
+ */
+public class ejer2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10;
+        int b=5;
+        int suma=a+b;
+        int resta=a-b;
+        int multiplicacion=a*b;
+        int division=a/b;
+        System.out.println("la suma es igual a:"+suma);
+        System.out.println("la resta es igual a:"+resta);
+        System.out.println("la multiplicacion es igual a:"+multiplicacion);
+        System.out.println("la division es igual a:"+division);
+    }
+    
+}
